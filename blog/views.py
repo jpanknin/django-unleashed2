f73b5c8a9975e5ee2fbb5fd35f857eb82e6983fc
jpanknin <jpanknin@gmail.com>
2016-05-08 20:24:10 -0400

    9.2 Complete

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 
 from .models import Post
+from .forms import PostForm
+
+
+class PostCreate(View):
+	form_class = PostForm
+	template_name = 'blog/post_form.html'
+
+	def get(self, request):
+		return render(request, self.template_name, {'form':self.form_class()})
+
+	def post(self, request):
+		bound_form = self.form_class(request.POST)
+		if bound_form.is_valid():
+			new_post = bound_form.save()
+			return redirect(new_post)
+
+		else:
+			return render(request, self.template_name, {'form':bound_form})
 
 def post_detail(request, year, month, slug):
 	post = get_object_or_404(Post, pub_date__year=year, pub_date__month=month, slug=slug)
--- organizer/views.py
+++ organizer/views.py
@@ -1,8 +1,20 @@
-# organizer.views
-
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import View
 
 from .models import Tag, Startup
+from .forms import TagForm, StartupForm
+from .utils import ObjectCreateMixin
+
+
+class NewsLinkCreate(ObjectCreateMixin, View):
+	form_class = NewsLinkForm
+	template_name = 'organizer/newslink_form.html'
+
+
+class StartupCreate(ObjectCreateMixin, View):
+	form_class = StartupForm
+	template_name = 'organizer/startup_form.html'
+
 
 def startup_list(request):
 	return render(request, 'organizer/startup_list.html', {'startup_list':Startup.objects.all()})
@@ -11,36 +23,15 @@ def startup_detail(request, slug):
 	startup = get_object_or_404(Startup, slug__iexact = slug)
 	return render(request, 'organizer/startup_detail.html', {'startup':startup})
 
+
+class TagCreate(ObjectCreateMixin, View):
+	form_class = TagForm
+	template_name = 'organizer/tag_form.html'
+
+
 def tag_list(request):
 	return render(request, 'organizer/tag_list.html', {'tag_list':Tag.objects.all()})
 
 def tag_detail(request, slug):
 	tag = get_object_or_404(Tag, slug__iexact = slug)
 	return render(request, 'organizer/tag_detail.html', {'tag':tag})
-
-	
-
-
-
-
-
-
-
-
-
-
-# homepage original code
-	# tag_list = Tag.objects.all()
-	# template = loader.get_template('organizer/tag_list.html')
-	# context = Context({'tag_list': tag_list})
-	# output = template.render(context)
-	# return HttpResponse(output)
-
-	# return render_to_response('organizer/tag_list.html', {'tag_list': Tag.objects.all()})
-
-# tag_detail original code
-	# template = loader.get_template('organizer/tag_detail.html')
-	# context = Context({'tag': tag})
-	# return HttpResponse(template.render(context))
-
-	# return render_to_response('organizer/tag_detail.html', {'tag': tag})
